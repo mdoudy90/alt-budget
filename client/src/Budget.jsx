7efd7329a9601f702d01f6c247a75236f8cdbfaf
whiskywolf88 <58891020+whiskywolf88@users.noreply.github.com>
2020-06-18 15:45:14 -0400

    add modularized front-end pages for budget app

---
+++ client/src/Budget.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+//should be stateful, will render view based on state
+
+class Budget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: "input",
+      item: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // renderView() {
+  //   switch (view) {
+  //     case "budget": {
+  //       //return budget google-reacts
+  //     }
+  //     case "input": {
+  //       //return input form below
+  //     }
+  //   }
+  // }
+
+  handleChange() {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [item]: value
+    })
+  }
+
+  render() {
+    return (
+      <div className="budgetform">Let's create a budget!
+        <form>
+          <label>
+            <input type="text" onChange={() => this.handleChange}></input>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Budget;
---
+++ client/src/Login.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from 'axios';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render() {
+    return (
+      <div className='login'>
+        <h1>Please Log In:</h1>
+        <form>
+          <label>Username:{'  '}
+            <input name='username' type='text' onChange={this.handleChange} />
+          </label>{'    '}
+          <label>Password:{'  '}
+            <input name='password' type='text' onChange={this.handleChange} />
+          </label>
+          <button onClick={() => this.sendLogin()}>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login;
