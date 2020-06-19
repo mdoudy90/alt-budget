5440a0df89221335b9256e0aa036ec2cde71eed7
whiskywolf88 <58891020+whiskywolf88@users.noreply.github.com>
2020-06-18 20:36:19 -0400

    add functionality to budget form, update nav links

--- client/src/Budget.jsx
+++ client/src/Budget.jsx
@@ -1,45 +1,76 @@
 import React from "react";
 
-//should be stateful, will render view based on state
-
 class Budget extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: "input",
-      item: ''
+      form: {
+        purchase: "",
+        cost: 0.0,
+        category: "",
+      },
+      transactions: this.props.transactions,
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  // renderView() {
-  //   switch (view) {
-  //     case "budget": {
-  //       //return budget google-reacts
-  //     }
-  //     case "input": {
-  //       //return input form below
-  //     }
-  //   }
-  // }
+  componentDidMount() {
+    this.props.getBudget();
+    //mount budget graph
+  }
 
   handleChange() {
     const target = event.target;
     const value = target.value;
     const name = target.name;
     this.setState({
-      [item]: value
-    })
+      [name]: value,
+    });
   }
 
   render() {
     return (
-      <div className="budgetform">Let's create a budget!
+      <div className="budgetform">
+        Tell us about your purchase:
         <form>
           <label>
-            <input type="text" onChange={() => this.handleChange}></input>
+            Purchase:{" "}
+            <input
+              name="purchase"
+              type="text"
+              value={this.state.item}
+              onChange={this.handleChange}
+            />
           </label>
+          {"   "}
+          <label>
+            {" "}
+            <input
+              name="cost"
+              type="number"
+              value={this.state.cost}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="text" list="categories" name="category" />
+          <datalist id="categories">
+            <option>Groceries</option>
+            <option>Food & Dining</option>
+            <option>Alcohol & Bars</option>
+            <option>Movies/TV</option>
+            <option>Cash/ATM</option>
+            <option>Transportation</option>
+            <option>Rent/Utilities</option>
+            <option>Miscellaneous</option>
+          </datalist>
         </form>
+        <button
+          onClick={() => {
+            this.props.addTransaction(this.state.form);
+          }}
+        >
+          Submit
+        </button>
       </div>
     );
   }
---
+++ client/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = props => (
+  <div>
+    <img className='hamilton' src='https://i5.walmartimages.com/asr/715a9d90-43fd-4451-ad11-ac2c04ca92ba_1.bbbcd0f6652ce7974c8ead44ea89c624.jpeg?odnHeight=2000&odnWidth=2000&odnBg=ffffff' alt='Piggy Bank - Quack!' width="600" height='600'></img>
+  </div>
+)
+
+export default Home;
