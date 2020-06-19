20f7e5fd15c17069d02d3c7363ec5f9b25c45ca1
Michael Doudy <michael.doudy@gmail.com>
2020-06-18 20:29:50 -0400

    added PieChart.jsx and dataParser helper

---
+++ client/src/helpers/dataParser.js
@@ -0,0 +1,21 @@
+const dataParser = (data) => {
+
+  let categoryData = {};
+  let transactions = data.transactions;
+
+  for (let key in transactions) {
+    if (transactions[key]['Transaction Type'] === 'debit') {
+      if (!categoryData[transactions[key].Category]) {
+        categoryData[transactions[key].Category] = Number(transactions[key].Amount);
+      } else {
+        categoryData[transactions[key].Category] += Number(transactions[key].Amount);
+      }
+    }
+  }
+
+  let sortedData = Object.entries(categoryData).sort((a, b) => b[1] - a[1]);
+
+  return sortedData.slice(0, 10);
+}
+
+export default dataParser;
